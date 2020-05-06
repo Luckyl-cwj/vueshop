ecd53f309889f5ea8c3ed879a21deb95a0c675bd
陈帅帅 <luckyl.cwj@gmail.com>
2020-05-06 14:34:33 +0800

    login

---
+++ src/components/home.vue
@@ -0,0 +1,15 @@
+<template>
+        <div >
+               luyouzhanweifu
+                
+            
+              </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less" scoped>
+    
+</style>
---
+++ src/plugins/element.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import {Form, Button,input,FormItem ,Message} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(input)
+Vue.use(FormItem)
+Vue.prototype.$message = Message
